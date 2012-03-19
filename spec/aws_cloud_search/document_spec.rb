79409271db1a257cd93ea310657afddf017c2dfd
David Jensen <david.jensen@spokesoftware.com>
2012-03-19 15:47:34 -0700

    RSpec test for the Document class.

---
+++ spec/aws_cloud_search/document_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe AwsCloudSearch::Document do
+  before(:each) do
+    @doc = AwsCloudSearch::Document.new
+  end
+
+  it "should accept values of the proper type" do
+    expect { @doc.id='123abc' }.to_not raise_error
+    expect { @doc.lang='en' }.to_not raise_error
+    expect { @doc.version=123 }.to_not raise_error
+  end
+
+  it "should throw an exception when given values of the wrong type" do
+    expect { @doc.id=123 }.to raise_error(Exception)
+    expect { @doc.lang=123 }.to raise_error(Exception)
+    expect { @doc.version='abc123' }.to raise_error(Exception)
+  end
+end
