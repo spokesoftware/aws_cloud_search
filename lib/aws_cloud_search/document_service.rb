3d1948820603b20245e07395480b5cf09f5bf303
David Jensen <david.jensen@spokesoftware.com>
2012-03-22 02:46:47 -0700

    Updated the document service to take a DocumentBatch and more directly implement the documents/search API.

--- lib/aws_cloud_search/document_service.rb
+++ lib/aws_cloud_search/document_service.rb
@@ -2,43 +2,23 @@ require "yajl/json_gem"
 require "aws_cloud_search"
 
 module AwsCloudSearch
+  # TODO: (dj) refactor into class CloudSearch
   class DocumentService
 
     def initialize(domain, region="us-east-1")
-      @batch_add = []
-      @batch_delete = []
       @conn = AwsCloudSearch::create_connection( AwsCloudSearch::document_url(domain, region) )
     end
 
-    def add_document(doc)
-      doc.type = 'add'
-      @batch_add << doc
-    end
-
-    # Adds a delete document operation to the batch. Removes lang and fields from the object as they are not
-    # required for delete operations.
-    # @param [Document] doc The document to delete
-    def delete_document(doc)
-      doc.type = 'delete'
-      doc.lang = nil
-      doc.clear_fields
-      @batch_delete << doc
-    end
-
-    # Sends the batch of document updates and deletes
-    def send_batch
-      json = (@batch_add + @batch_delete).map {|item| item.to_hash}.to_json
+    # Sends a batch of document updates and deletes by invoking the CloudSearch documents/batch API
+    def documents_batch(doc_batch)
+      raise ArgumentError.new("Invalid argument. Expected DocumentBatch, got #{doc_batch.class}.") unless doc_batch.is_a? DocumentBatch
 
       resp = @conn.post do |req|
         req.url "/#{AwsCloudSearch::API_VERSION}/documents/batch"
         req.headers['Content-Type'] = 'application/json'
-        req.body = json
+        req.body = doc_batch.to_json
       end
       raise(Exception, "AwsCloudSearchCloud::DocumentService batch returned #{resp.body[:errors].size} errors: #{resp.body[:errors].join(';')}") if resp.body[:status] == 'error'
-    end
-
-    def batch_size
-      @batch_add.size + @batch_delete.size
     end
 
   end
