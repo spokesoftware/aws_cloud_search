4c391701b94d0a92c5f35ec6b434cf052500c5f7
David Jensen <david.jensen@spokesoftware.com>
2012-03-22 02:44:16 -0700

    Created the DocumentBatch class for bundling up document adds and deletes.

---
+++ lib/aws_cloud_search/document_batch.rb
@@ -0,0 +1,72 @@
+require "yajl/json_gem"
+require "aws_cloud_search"
+
+module AwsCloudSearch
+  class DocumentBatch
+
+    attr_reader :bytesize
+
+    # Constructor
+    # @param [Integer] pref_bytesize The preferred size of the batch in bytes. May be exceeded, if so batch is considered full.
+    # @param [Integer] max_bytesize The batch size in bytes must not exceed this number. Must be greater than pref_bytesize.
+    # @raise [ArgumentError] If pref_bytesize is not less than max_bytesize
+    def initialize(pref_bytesize=1048576, max_bytesize= 5242880)
+      raise ArgumentError.new("pref_bytesize must be less than max_bytesize") if pref_bytesize >= max_bytesize
+
+      @pref_bytesize = pref_bytesize
+      @max_bytesize = max_bytesize
+      @batch_add = []
+      @batch_delete = []
+      @bytesize = 0
+    end
+
+    # Adds a document with the add operation to the batch.
+    # @param [Document] doc
+    # @raise [ArgumentError] If parameter is not an AwsCloudSearch::Document
+    def add_document(doc)
+      raise ArgumentError.new("Invalid Type") unless doc.is_a? Document
+
+      doc.type = 'add'
+      doc_bytesize = doc.to_json.bytesize
+
+      raise Exception.new("Max batch size exceeded, document add was not added to batch.") if (doc_bytesize + @bytesize) > @max_bytesize
+
+      @bytesize += doc_bytesize
+      @batch_add << doc
+    end
+
+    # Adds a delete document operation to the batch. Removes lang and fields from the object as they are not
+    # required for delete operations.
+    # @param [Document] doc The document to delete
+    # @raise [ArgumentError] If parameter is not an AwsCloudSearch::Document
+    def delete_document(doc)
+      raise ArgumentError.new("Invalid Type") unless doc.is_a? Document
+
+      doc.type = 'delete'
+      doc.lang = nil
+      doc.clear_fields
+      doc_bytesize = doc.to_json.bytesize
+
+      raise Exception.new("Max batch size exceeded, document delete was not added to batch.") if (doc_bytesize + @bytesize) > @max_bytesize
+
+      @bytesize += doc_bytesize
+      @batch_delete << doc
+    end
+
+    # @return [Integer] Number of items in the batch
+    def size
+      @batch_add.size + @batch_delete.size
+    end
+
+    # @return [Boolean] True if the bytesize of the batch exceeds the preferred bytesize
+    def full?
+      @bytesize >= @pref_bytesize
+    end
+
+    # @return [String] The JSON string representation of the DocumentBatch
+    def to_json
+      (@batch_add + @batch_delete).map {|item| item.to_hash}.to_json
+    end
+
+  end
+end
---
+++ spec/aws_cloud_search/document_batch_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+describe AwsCloudSearch::DocumentBatch do
+
+  before(:each) do
+    @doc1 = AwsCloudSearch::Document.new(true)
+    @doc1.id = '73e'
+    @doc1.lang = 'en'
+    @doc1.add_field('name', 'Jane Williams')
+    @doc1.add_field('type', 'person')
+
+    @doc2 = AwsCloudSearch::Document.new(true)
+    @doc2.id = '47p'
+
+    @batch = AwsCloudSearch::DocumentBatch.new
+  end
+
+  it "should should not instantiate" do
+    expect { AwsCloudSearch::DocumentBatch.new(100, 100) }.to raise_error(ArgumentError)
+    expect { AwsCloudSearch::DocumentBatch.new(101, 100) }.to raise_error(ArgumentError)
+  end
+
+  it "should instantiate" do
+    expect { AwsCloudSearch::DocumentBatch.new }.to_not raise_error
+    expect { AwsCloudSearch::DocumentBatch.new(100, 101) }.to_not raise_error
+  end
+
+  it "should raise error when passed an invalid object type" do
+    expect { @batch.add_document("Hello") }.to raise_error(ArgumentError)
+    expect { @batch.delete_document("Hello") }.to raise_error(ArgumentError)
+  end
+
+  it "should not raise error when passed a Document" do
+    expect { @batch.add_document(@doc1) }.to_not raise_error(ArgumentError)
+    expect { @batch.delete_document(@doc1) }.to_not raise_error(ArgumentError)
+  end
+
+  it "should return the correct size" do
+    @batch.add_document @doc1
+    @batch.size.should eq(1)
+
+    @batch.delete_document @doc1
+    @batch.size.should eq(2)
+  end
+
+  it "should raise error when the max batch size is exceeded" do
+    small_batch = AwsCloudSearch::DocumentBatch.new(1, 10)
+    expect {small_batch.add_document @doc1}.to raise_error
+    expect {small_batch.delete_document @doc1}.to raise_error
+  end
+
+  it "should be full" do
+    bytesize = @doc1.to_json.bytesize
+
+    b1 = AwsCloudSearch::DocumentBatch.new(bytesize)
+    b1.add_document @doc1
+    b1.full?.should be_true
+
+    b2 = AwsCloudSearch::DocumentBatch.new(bytesize-1)
+    b2.add_document @doc1
+    b2.full?.should be_true
+
+    bytesize = @doc2.to_json.bytesize
+
+    b3 = AwsCloudSearch::DocumentBatch.new(bytesize)
+    b3.delete_document @doc2
+    b3.full?.should be_true
+
+    b4 = AwsCloudSearch::DocumentBatch.new(bytesize-1)
+    b4.delete_document @doc2
+    b4.full?.should be_true
+  end
+
+  it "should return the total bytesize of all docs" do
+    @batch.bytesize.should eq(0)
+
+    @batch.add_document @doc1
+    bytesize = @doc1.to_json.bytesize
+    @batch.bytesize.should eq(bytesize)
+
+    @batch.delete_document @doc2
+    bytesize += @doc2.to_json.bytesize
+    @batch.bytesize.should eq(bytesize)
+  end
+
+  it "should not be full" do
+    @batch.add_document @doc1
+    @batch.full?.should_not be_true
+
+    @batch.delete_document @doc1
+    @batch.full?.should_not be_true
+  end
+
+end
