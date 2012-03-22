e004b7a021ddb7f4feb964d6f6433c4e2bb5d02f
David Jensen <david.jensen@spokesoftware.com>
2012-03-22 02:43:21 -0700

    Placeholder for the DocumentBatcher convenience class.

---
+++ lib/aws_cloud_search/document_batcher.rb
@@ -0,0 +1,11 @@
+require "yajl/json_gem"
+require "aws_cloud_search"
+
+module AwsCloudSearch
+  # Convenience method that will allow continuous batch additions and will chunk to a size threshold
+  # and send requests for each chunk.
+  class DocumentBatcher
+
+  end
+
+end
