3d52e831956329270922e70ac8ed9daf865af813
Mike Javorski <mike.javorski@spoke.com>
2012-03-26 15:00:10 -0700

    refactor cloud_search_spec: move ds into a let() and move doc1 and batch into the example which uses them

--- spec/aws_cloud_search/cloud_search_spec.rb
+++ spec/aws_cloud_search/cloud_search_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
 describe AwsCloudSearch::CloudSearch do
-  before(:each) do
-    @ds = AwsCloudSearch::CloudSearch.new('spoke-dev-1-na4eszv5wms3lahf4xnq27x6ym')
 
-    @doc1 = AwsCloudSearch::Document.new(true)
-    @doc1.id = '73e'
-    @doc1.lang = 'en'
-    @doc1.add_field('name', 'Jane Williams')
-    @doc1.add_field('type', 'person')
-
-    @batch = AwsCloudSearch::DocumentBatch.new
-  end
+  let(:ds) { AwsCloudSearch::CloudSearch.new('spoke-dev-1-na4eszv5wms3lahf4xnq27x6ym') }
 
   it "should send document batch" do
-    @batch.add_document @doc1
-    expect { @ds.documents_batch(@batch) }.to_not raise_error
+    batch = AwsCloudSearch::DocumentBatch.new
+    
+    doc1 = AwsCloudSearch::Document.new(true)
+    doc1.id = '73e'
+    doc1.lang = 'en'
+    doc1.add_field('name', 'Jane Williams')
+    doc1.add_field('type', 'person')
+
+    batch.add_document doc1
+    ds.documents_batch(batch)
   end
 
 end
