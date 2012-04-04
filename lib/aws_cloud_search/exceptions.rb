0cdeff076b48092c65d161bdea9226eb9abe315e
David Jensen <david.jensen@spokesoftware.com>
2012-04-03 23:47:40 -0700

    Added exception classes.

---
+++ lib/aws_cloud_search/exceptions.rb
@@ -0,0 +1,17 @@
+module AwsCloudSearch
+  # Cloud Search API errors
+  class AwsCloudSearchError < StandardError; end
+  class WildcardTermLimit < AwsCloudSearchError; end
+  class InvalidFieldOrRankAliasInRankParameter < AwsCloudSearchError; end
+  class UnknownFieldInMatchExpression < AwsCloudSearchError; end
+  class IncorrectFieldTypeInMatchExpression < AwsCloudSearchError; end
+  class InvalidMatchExpression < AwsCloudSearchError; end
+  class UndefinedField < AwsCloudSearchError; end
+
+  # HTTP errors
+  class UnexpectedHTTPException < StandardError; end
+  class HttpClientError < StandardError; end
+  class HttpServerError < StandardError; end
+  class RequestTimeout < HttpClientError; end
+  class BandwidthLimitExceeded < HttpServerError; end
+end
